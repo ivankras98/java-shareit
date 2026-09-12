dfc6cf00824c466b84c13fcc30e8edc8e67a4dcf
Ivan Krasilnikiov <ikra-nn@yandex.ru>
2026-09-12 12:24:11 +0300

    Fix checkstyle: avoid leading brace regex violation in JSON test string

--- server/src/test/java/ru/practicum/shareit/booking/dto/BookingDtoJsonTest.java
+++ server/src/test/java/ru/practicum/shareit/booking/dto/BookingDtoJsonTest.java
@@ -30,9 +30,7 @@ class BookingDtoJsonTest {
 
     @Test
     void deserialize_shouldParseDatesCorrectly() throws Exception {
-        String content = """
-        {"itemId": 1, "start": "2026-09-15T10:00:00", "end": "2026-09-16T10:00:00"}
-        """;
+        String content = "{\"itemId\": 1, \"start\": \"2026-09-15T10:00:00\", \"end\": \"2026-09-16T10:00:00\"}";
 
         BookingDto result = json.parse(content).getObject();
 
