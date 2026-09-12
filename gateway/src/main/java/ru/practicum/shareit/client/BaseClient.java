27fc8a0ae8edb653cdfffa374630ae3434f918ad
Ivan Krasilnikiov <ikra-nn@yandex.ru>
2026-09-12 12:17:53 +0300

    Fix checkstyle: add missing blank line in BaseClient

--- gateway/src/main/java/ru/practicum/shareit/client/BaseClient.java
+++ gateway/src/main/java/ru/practicum/shareit/client/BaseClient.java
@@ -50,6 +50,7 @@ public class BaseClient {
     protected <T> ResponseEntity<Object> patch(String path, Long userId, @Nullable Map<String, Object> parameters, T body) {
         return makeAndSendRequest(HttpMethod.PATCH, path, userId, parameters, body);
     }
+
     protected ResponseEntity<Object> delete(String path) {
         return delete(path, null, null);
     }
